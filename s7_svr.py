c00c17ae64dcbb4068820f2e503e051c9eb51db6
Mark Beveridge <mark@dreamtime.org>
2018-06-16 19:41:09 +0100

    SVR - Business Problem solutions (Python; R)

---
+++ s7_svr.py
@@ -0,0 +1,53 @@
+# SVR
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data/s7_Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2:3].values
+# NB. Answer sheet had `y = dataset.iloc[:, 2].values` (and so did video) but that gave horiz line again
+# We were told previously that `X` had to be a matrix and `y` a vector
+# So although the above 'hack' gives the correct curve, I'm unclear if/why it's valid
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling [§7 Lect67: "SVR in Python" ...from 6min30]
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# Fitting SVR to the dataset [§7 Lect67: "SVR in Python"]
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+# Predicting a new result [§7 Lect67: "SVR in Python"]
+# ...but this code is NOT what is in video 13'50-17'20. And it gives a different $prediction value
+y_pred = regressor.predict(6.5)
+y_pred = sc_y.inverse_transform(y_pred)
+
+# Visualising the SVR results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
