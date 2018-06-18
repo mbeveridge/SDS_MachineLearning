8f687eb3115e480d6613e8f886532f2a68e0b04c
Mark Beveridge <mark@dreamtime.org>
2018-06-18 22:50:51 +0100

    Decision Tree Regression - Business Problem solutions (Python; R)

---
+++ s8_decision_tree_regression.py
@@ -0,0 +1,54 @@
+# Decision Tree Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data/s8_Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Decision Tree Regression to the dataset [§8 Lect71: "Decision Tree Regression in Python"]
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+# Visualising the Decision Tree Regression results
+# [§8 Lect71: "Decision Tree Regression in Python" 7'20-10'50] explains we can't use this, because
+# we are now dealing with a non-continuous regression model (where the plot should be a series of steps)
+"""plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()"""
+
+# Visualising the Decision Tree Regression results (higher resolution)
+# [§8 Lect71: "Decision Tree Regression in Python" 11'30-12'15] says why we use this code instead
+# [13'30-13'50] says the Intuition was 2-D, this example & plot is 1-D, but Decision Tree Regression
+# is only really useful when we have multiple independent variables (ie. more than 1-D)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
