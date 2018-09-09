eed3bea2dfa86f7186a8298c6ecad8bbfd5e0a57
Mark Beveridge <mark@dreamtime.org>
2018-09-09 13:11:01 +0100

    MLR - solutions (Python; R), after Lect +1

--- s05_multiple_linear_regression.R
+++ s05_multiple_linear_regression.R
@@ -4,7 +4,7 @@
 dataset = read.csv('data/s5_50_Startups.csv')
 # dataset = dataset[, 2:3]
 
-# Encoding categorical data [§5 Lect47: "MLR - R pt1" ...@3min00]
+# Encoding categorical data [§5 Lect48: "MLR - R pt1" ...@3min00]
 dataset$State = factor(dataset$State,
                        levels = c('New York', 'California', 'Florida'),
                        labels = c(1, 2, 3))
@@ -21,14 +21,14 @@ test_set = subset(dataset, split == FALSE)
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Multiple Linear Regression to the Training set [§5 Lect48: "MLR - R pt2"]
+# Fitting Multiple Linear Regression to the Training set [§5 Lect49: "MLR - R pt2"]
 regressor = lm(formula = Profit ~ .,
                data = training_set)
 
-# Predicting the Test set results [§5 Lect49: "MLR - R pt3"]
+# Predicting the Test set results [§5 Lect50: "MLR - R pt3"]
 y_pred = predict(regressor, newdata = test_set)
 
-# Building the optimal model using Backward Elimination [§5 Lect50: "MLR - R (Backward Elimination): HOMEWORK !"]
+# Building the optimal model using Backward Elimination [§5 Lect51: "MLR - R (Backward Elimination): HOMEWORK !"]
 regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
                data = dataset)
 summary(regressor)
@@ -39,7 +39,7 @@ summary(regressor)
 regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
                data = dataset)
 summary(regressor)
-# cont'd [§5 Lect51: "MLR - R (Backward Elimination): Homework solution"]
+# cont'd [§5 Lect52: "MLR - R (Backward Elimination): Homework solution"]
 regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
                data = dataset)
 summary(regressor)
--- s05_multiple_linear_regression.py
+++ s05_multiple_linear_regression.py
@@ -10,14 +10,14 @@ dataset = pd.read_csv('data/s5_50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-# Encoding categorical data [§5 Lect40: "MLR - Python pt1" ...@6min50]
+# Encoding categorical data [§5 Lect41: "MLR - Python pt1" ...@6min50]
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
-# Avoiding the Dummy Variable Trap [§5 Lect40: "MLR - Python pt1" ...@12min20]
+# Avoiding the Dummy Variable Trap [§5 Lect41: "MLR - Python pt1" ...@12min20]
 # [...but @13min20 says LinearRegression library takes care of it, so not needed (but can run if want to)]
 X = X[:, 1:]
 
@@ -33,25 +33,25 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Multiple Linear Regression to the Training set [§5 Lect41: "MLR - Python pt2"]
+# Fitting Multiple Linear Regression to the Training set [§5 Lect42: "MLR - Python pt2"]
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-# Predicting the Test set results [§5 Lect42: "MLR - Python pt3"]
+# Predicting the Test set results [§5 Lect43: "MLR - Python pt3"]
 y_pred = regressor.predict(X_test)
 
-# Building the optimal model using Backward Elimination [§5 Lect43: "MLR - Python (Backward Elimination): Prep"]
+# Building the optimal model using Backward Elimination [§5 Lect44: "MLR - Python (Backward Elimination): Prep"]
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
-# cont'd [§5 Lect44: "MLR - Python (Backward Elimination): HOMEWORK !"]
+# cont'd [§5 Lect45: "MLR - Python (Backward Elimination): HOMEWORK !"]
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit() # "intercept [b0] is not included by default and should be added"
 regressor_OLS.summary()
 X_opt = X[:, [0, 1, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-# cont'd [§5 Lect45: "MLR - Python (Backward Elimination): Homework solution"]
+# cont'd [§5 Lect46: "MLR - Python (Backward Elimination): Homework solution"]
 X_opt = X[:, [0, 3, 4, 5]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
