0615e24da5f9189ba0bae93fd398f2ca26ae0d13
Mark Beveridge <mark@dreamtime.org>
2018-09-09 13:14:34 +0100

    Polynomial Regression - solutions (Python; R), after Lect +1

--- s06_polynomial_regression.R
+++ s06_polynomial_regression.R
@@ -17,18 +17,18 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Linear Regression to the dataset [§6 Lect61: "Polynomial Regression - R pt2" ...for comparison]
+# Fitting Linear Regression to the dataset [§6 Lect62: "Polynomial Regression - R pt2" ...for comparison]
 lin_reg = lm(formula = Salary ~ .,
              data = dataset)
 
-# Fitting Polynomial Regression to the dataset [§6 Lect61: "Polynomial Regression - R pt2"]
+# Fitting Polynomial Regression to the dataset [§6 Lect62: "Polynomial Regression - R pt2"]
 dataset$Level2 = dataset$Level^2 # Add a column (called "Level2") to the dataframe, which is Level^2
 dataset$Level3 = dataset$Level^3
-dataset$Level4 = dataset$Level^4 # Video eventually says (Lect62 17'30-18'40) to include degree = 4
+dataset$Level4 = dataset$Level^4 # Video eventually says (Lect63 17'30-18'40) to include degree = 4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
 
-# Visualising the Linear Regression results [§6 Lect62: "Polynomial Regression - R pt3" ...for comparison]
+# Visualising the Linear Regression results [§6 Lect63: "Polynomial Regression - R pt3" ...for comparison]
 # install.packages('ggplot2')
 library(ggplot2)
 ggplot() +
@@ -40,7 +40,7 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Visualising the Polynomial Regression results [§6 Lect62: "Polynomial Regression - R pt3"]
+# Visualising the Polynomial Regression results [§6 Lect63: "Polynomial Regression - R pt3"]
 # install.packages('ggplot2')
 library(ggplot2)
 ggplot() +
@@ -53,7 +53,7 @@ ggplot() +
   ylab('Salary')
 
 # Visualising the Regression Model results (add x_grid ...if we want higher resolution and smoother curve)
-# [§6 Lect62: "Polynomial Regression - R pt3" at 19'30 says this will be in the file (but not in video)]
+# [§6 Lect63: "Polynomial Regression - R pt3" at 19'30 says this will be in the file (but not in video)]
 # install.packages('ggplot2')
 library(ggplot2)
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
@@ -70,10 +70,10 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Predicting a new result with Linear Regression [§6 Lect63: "Polynomial Regression - R pt4" ...for comparison]
+# Predicting a new result with Linear Regression [§6 Lect64: "Polynomial Regression - R pt4" ...for comparison]
 y_pred = predict(lin_reg, data.frame(Level = 6.5)) # This is now a single prediction, not a vector
 
-# Predicting a new result with Polynomial Regression [§6 Lect63: "Polynomial Regression - R pt4"]
+# Predicting a new result with Polynomial Regression [§6 Lect64: "Polynomial Regression - R pt4"]
 y_pred = predict(poly_reg, data.frame(Level = 6.5,
                              Level2 = 6.5^2,
                              Level3 = 6.5^3,
--- s06_polynomial_regression.py
+++ s06_polynomial_regression.py
@@ -21,12 +21,12 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
 
-# Fitting Linear Regression to the dataset [§6 Lect56: "Polynomial Regression - Python pt2" ...for comparison]
+# Fitting Linear Regression to the dataset [§6 Lect57: "Polynomial Regression - Python pt2" ...for comparison]
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
-# Fitting Polynomial Regression to the dataset [§6 Lect56: "Polynomial Regression - Python pt2"]
+# Fitting Polynomial Regression to the dataset [§6 Lect57: "Polynomial Regression - Python pt2"]
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4) # Video eventually says (~15'00-17'10) to use degree = 4, not degree = 2
@@ -37,7 +37,7 @@ X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-# Visualising the Linear Regression results [§6 Lect57: "Polynomial Regression - Python pt3" ...for comparison]
+# Visualising the Linear Regression results [§6 Lect58: "Polynomial Regression - Python pt3" ...for comparison]
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Linear Regression)')
@@ -45,7 +45,7 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Polynomial Regression results [§6 Lect57: "Polynomial Regression - Python pt3"]
+# Visualising the Polynomial Regression results [§6 Lect58: "Polynomial Regression - Python pt3"]
 plt.scatter(X, y, color = 'red')
 # plt.plot(X, lin_reg_2.predict(X_poly), color = 'blue')
 plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
@@ -55,7 +55,7 @@ plt.ylabel('Salary')
 plt.show()
 
 # Visualising the Polynomial Regression results (add X_grid ...if we want higher resolution and smoother curve)
-# [§6 Lect57: "Polynomial Regression - Python pt3" from 18min00]
+# [§6 Lect58: "Polynomial Regression - Python pt3" from 18min00]
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1)) # to turn a vector into a matrix
 plt.scatter(X, y, color = 'red')
@@ -65,8 +65,8 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Predicting a new result with Linear Regression [§6 Lect58: "Polynomial Regression - Python pt4" ...for comparison]
+# Predicting a new result with Linear Regression [§6 Lect59: "Polynomial Regression - Python pt4" ...for comparison]
 lin_reg.predict(6.5)
 
-# Predicting a new result with Polynomial Regression [§6 Lect58: "Polynomial Regression - Python pt4"]
+# Predicting a new result with Polynomial Regression [§6 Lect59: "Polynomial Regression - Python pt4"]
 lin_reg_2.predict(poly_reg.fit_transform(6.5))
